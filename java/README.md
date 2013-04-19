d45e29e38286150e246de5d6526da578b021a0ec
Laura Luiz <laura.luiz@commercetools.de>
2013-04-19 17:25:03 +0200

    Add Java example.

---
+++ java/README.md
@@ -0,0 +1,13 @@
+Java Hello on SPHERE.IO
+=======================
+
+A Java example using Sphere Java Client to list all products in your project.
+
+## Run
+
+Add your project key, OAuth client ID and client secret to `config.properties`.
+
+```bash
+$ gradle run
+```
+
---
+++ java/src/helloApi.java
@@ -0,0 +1,55 @@
+import io.sphere.client.SphereException;
+import io.sphere.client.shop.ShopClient;
+import io.sphere.client.shop.ShopClientConfig;
+import io.sphere.client.shop.ApiMode;
+import io.sphere.client.shop.model.Product;
+import io.sphere.client.model.SearchResult;
+
+import java.util.List;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.ConversionException;
+
+public class helloApi {
+    public static void main(String[] args) {
+        try {
+            Configuration config = readConfiguration();       
+        	
+            ShopClient client = buildClient(
+                config.getString("projectKey"),
+                config.getString("clientId"),
+                config.getString("clientSecret"));
+
+        	listProducts(client);
+
+        } catch (ConfigurationException ce) {
+            System.err.println("Could not open configuration file 'config.properties'");
+        } catch (ConversionException ce) {
+            System.err.println("Could not read configuration values");
+        } catch (SphereException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Configuration readConfiguration() throws ConfigurationException {
+        return new PropertiesConfiguration("config.properties");
+    }
+
+    public static ShopClient buildClient(String project, String client, String secret) {
+        ShopClientConfig config = new ShopClientConfig
+                .Builder(project, client, secret)
+                .setApiMode(ApiMode.Live)
+                .build();
+        return ShopClient.create(config);
+    }
+
+    public static void listProducts(ShopClient client) {
+        SearchResult<Product> result = client.products().all().pageSize(1000).fetch();
+        for (Product product : result.getResults()) {
+            System.out.println(product.getName());
+        }
+    }
+}
