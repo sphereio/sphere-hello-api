4887846c79b2d2db8202053a26a905ca1022295b
Martin Konicek <martin.konicek@gmail.com>
2013-05-08 17:32:31 +0300

    Formatting.

--- java/src/HelloApi.java
+++ java/src/HelloApi.java
@@ -3,7 +3,8 @@ import io.sphere.client.shop.model.Product;
 
 import java.io.IOException;
 
-// This example uses the official Sphere Java client: https://github.com/commercetools/sphere-play-sdk
+// This example uses the official Sphere Java client:
+// https://github.com/commercetools/sphere-play-sdk
 
 public class HelloApi {
     public static void main(String[] args) throws IOException {
