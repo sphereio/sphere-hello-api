cb125fc9b143e183cb4bb3b6041a643e3e350559
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2013-11-06 23:14:16 +0100

    Switched to build.sbt and also made small improvements in the example app

--- scala/src/main/scala/SphereHello.scala
+++ scala/src/main/scala/SphereHello.scala
@@ -7,7 +7,7 @@ import com.ning.http.util.Base64
 import dispatch._
 import net.liftweb.json._
 
-object SphereHello {
+object SphereHello extends App {
   val api = "https://api.sphere.io/"
   val auth_api = "https://auth.sphere.io/oauth/token"
 
@@ -28,29 +28,27 @@ object SphereHello {
       .setBody(s"grant_type=client_credentials&scope=manage_project:${projectKey}")
     ).map(rsp => parse(rsp.getResponseBody) \ "access_token" match {
       case JString(s) => s
-      case _ => throw new IllegalStateException("Auth response does not contain 'access_token'.")
+      case _ => throw new IllegalStateException("Auth response does not contain 'access_token'.\n" + rsp.getResponseBody)
     })
   }
   
-  /** Issues a query to get all products, see http://sphere.io/dev/HTTP_API_Projects_Products.html#product-projections-by-query.
-    * @return <a href="HTTP_API.html#paged-query-response">PagedQueryResult</a> 
+  /** Issues a query to get all products, see [[http://sphere.io/dev/HTTP_API_Projects_Products.html#product-projections-by-query]].
+    * @return <a href="HTTP_API.html#paged-query-response">PagedQueryResult</a>
     *         with the <code>results</code> array of <a href="#product-projection">ProductProjection</a> JSON. */
   def getProducts(oauthToken: String): Future[JValue] = {
     h(url(s"${api}${projectKey}/product-projections").setHeader("Authorization", s"Bearer ${oauthToken}"))
       .map(rsp => parse(rsp.getResponseBody))
   }
-  
-  def main(args: Array[String]) = {
 
-    val f = for {
-      token <- login(clientId, clientSecret, projectKey)
-      products <- getProducts(token)
-    } yield products \ "total" match {
-        case JInt(n) => n
-        case _ => throw new IllegalStateException("Product query response does not contain 'total'.")
-      }
-    val nrOfProducts = Await.result(f, Duration.Inf)
-    h.shutdown()
-    println("Number of products: " + nrOfProducts)
-  } 
+  val f = for {
+    token <- login(clientId, clientSecret, projectKey)
+    products <- getProducts(token)
+  } yield products \ "total" match {
+    case JInt(n) => n
+    case _ => throw new IllegalStateException("Product query response does not contain 'total'.")
+  }
+
+  val nrOfProducts = Await.result(f, Duration.Inf)
+  h.shutdown()
+  println("Number of products: " + nrOfProducts)
 }
