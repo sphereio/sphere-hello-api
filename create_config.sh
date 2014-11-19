9aafad4615c369f03180c5809b16b5fcbb13196d
Michael Schleichardt <michael@schleichardt.net>
2014-11-19 08:53:20 +0100

    Add travis build for curl. #7

--- create_config.sh
+++ create_config.sh
@@ -5,3 +5,9 @@ projectKey = $PROJECT_KEY
 clientId = $CLIENT_ID
 clientSecret = $CLIENT_SECRET
 EOF
+
+cat > "curl/config" << EOF
+CLIENT_ID="$CLIENT_ID"
+CLIENT_SECRET="$CLIENT_SECRET"
+PROJECT_KEY="$PROJECT_KEY"
+EOF
