77d10246b73e3e758cd91f7a9f5b4ff688ab43ae
Nicola Molinari <nicola.molinari@commercetools.de>
2014-11-20 16:01:32 +0100

    Reference new repo

--- node-js/config.js
+++
@@ -1,4 +0,0 @@
-// SPHERE.IO application settings
-exports.client_id = 'client_id'; // Your application client id
-exports.client_secret = 'client_secret'; // Your application client secret
-exports.project_key = "project_key"; // Your project key
