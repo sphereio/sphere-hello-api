216a65d2c37caf9a3184957a2cce6d51d5647296
Michael Schleichardt <michael@schleichardt.net>
2014-11-26 18:21:20 +0100

    First attempt for python3. #14

--- create_config.sh
+++ create_config.sh
@@ -6,7 +6,13 @@ clientId = $CLIENT_ID
 clientSecret = $CLIENT_SECRET
 EOF
 
-cat > "python/config.py" << EOF
+cat > "python2/config.py" << EOF
+PROJECT_KEY = "$PROJECT_KEY"
+CLIENT_ID = "$CLIENT_ID"
+CLIENT_SECRET = "$CLIENT_SECRET"
+EOF
+
+cat > "python3/config.py" << EOF
 PROJECT_KEY = "$PROJECT_KEY"
 CLIENT_ID = "$CLIENT_ID"
 CLIENT_SECRET = "$CLIENT_SECRET"
