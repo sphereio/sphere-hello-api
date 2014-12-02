0df6dd2f7cf54674b9b8b722e4b8f92d37560d97
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2014-12-02 09:58:40 +0100

    Changed Java version check to groovy -version output The travis Box seems to have multiple JVM versions in the path? however...

--- groovy/runtest.sh
+++ groovy/runtest.sh
@@ -3,14 +3,19 @@
 set -x
 # return error for whole script if any command returns error (append " || true" to override that for a line):
 set -e
+# clear the fog about what's in the path:
+java -version || true
+
 # make sure groovy is there
 sudo apt-get install groovy
-# print groovy version for easier debugging of the build:
+
+# print groovy version for transparency. cross-check the java version
 groovy -version || true
+java -version || true
 
 # our travis is running on JVM8, too, but Groovy only supports JVM8 since Groovy 2.3, which isn't available on travis.)
 # the second line returns 
-JAVA_VER=$(java -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+GROOVY_JVM=$(groovy -version 2>&1 | sed 's/.*JVM: \(.*\)\.\(.*\)\..*/\1\2/; 1q')
 
-# if java is max 1.7, get stuff and grep for the result
-[ "$JAVA_VER" -ge 18 ] && echo "groovy is not compatible with JDK8 yet, skipping test" || groovy ./apidump.groovy | grep "MB PREMIUM TECH T"
+# if groovy JVM is max 1.7, get stuff and grep for the result
+[ "$GROOVY_JVM" -ge 18 ] && echo "groovy is not compatible with JDK8 yet, skipping test" || groovy ./apidump.groovy | grep "MB PREMIUM TECH T"
