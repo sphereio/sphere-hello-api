599d0bf9c726e4c5a0531711197fbbb5a69205b9
schleichardt <michael.schleichardt@commercetools.de>
2016-01-05 16:47:37 +0100

    Change folder structure for Java project.

--- java/src/main/java/HelloApi.java
+++ java/src/main/java/HelloApi.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class HelloApi {
     public static void main(String[] args) throws IOException {
-        SphereClient sphere = SphereClient.create(Config.load("config.properties"));
+        SphereClient sphere = SphereClient.create(Config.load("src/main/resources/config.properties"));
 
         for (Product product : sphere.products().all().fetch().getResults()) {
             System.out.println(product.getName());
