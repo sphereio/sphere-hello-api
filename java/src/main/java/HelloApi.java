ccd54662937badb8430f599ca01e1f59d7d21041
schleichardt <michael.schleichardt@commercetools.de>
2016-01-05 17:14:05 +0100

    Migrate Java example to JVM SDK. close #35, close sphereio/sphere-jvm-sdk#896

--- java/src/main/java/HelloApi.java
+++ java/src/main/java/HelloApi.java
@@ -1,18 +1,44 @@
-import io.sphere.client.shop.SphereClient;
-import io.sphere.client.shop.model.Product;
+import io.sphere.sdk.client.BlockingSphereClient;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.client.SphereClientConfig;
+import io.sphere.sdk.client.SphereClientFactory;
+import io.sphere.sdk.products.ProductProjection;
+import io.sphere.sdk.products.search.ProductProjectionSearch;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 // This example uses the official Sphere Java client:
-// https://github.com/commercetools/sphere-play-sdk
+// https://github.com/sphereio/sphere-jvm-sdk
 
 public class HelloApi {
-    public static void main(String[] args) throws IOException {
-        SphereClient sphere = SphereClient.create(Config.load("src/main/resources/commercetools.properties"));
-
-        for (Product product : sphere.products().all().fetch().getResults()) {
-            System.out.println(product.getName());
+    public static void main(String[] args) throws Exception {
+        final SphereClientConfig config = loadCommercetoolsConfig();
+        final SphereClientFactory factory = SphereClientFactory.of();
+        final SphereClient underlyingClient = factory.createClient(config);//this client only works asynchronous
+        try(final BlockingSphereClient client = BlockingSphereClient.of(underlyingClient, 10, TimeUnit.SECONDS)) {
+            final ProductProjectionSearch sphereRequest = ProductProjectionSearch.ofCurrent()
+                    .withSort(m -> m.createdAt().byDesc());
+            final List<ProductProjection> products = client.executeBlocking(sphereRequest).getResults();
+            for (ProductProjection product : products) {
+                System.out.println(product.getName());
+            }
         }
-        sphere.shutdown();
+    }
+
+    private static SphereClientConfig loadCommercetoolsConfig() throws IOException {
+        final Properties properties = loadPropertiesFromClasspath("commercetools.properties");
+        return SphereClientConfig.ofProperties(properties, "");
+    }
+
+    private static Properties loadPropertiesFromClasspath(final String path) throws IOException {
+        final Properties properties = new Properties();
+        try (final InputStream stream = HelloApi.class.getResourceAsStream(path)) {
+            properties.load(stream);
+        }
+        return properties;
     }
 }
